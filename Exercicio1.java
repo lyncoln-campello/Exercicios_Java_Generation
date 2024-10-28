dd5250bbc2ff533420a8dbeb80cd065021226a45
Lyncoln Campello <lyncolncamp@gmail.com>
2024-10-28 16:47:53 -0300

    first commit

---
+++ Exercicio1.java
@@ -0,0 +1,24 @@
+package exercicio1;
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+
+	;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		Scanner leia = new Scanner(System.in);
+	
+		float salario, abono, novoSalario;
+		System.out.print("Digite o salário:  ");
+		salario = leia.nextFloat();
+		System.out.print("Digite o abono: ");
+		abono = leia.nextFloat();
+		novoSalario = salario + abono;
+		System.out.print("O Novo salário é: "+ novoSalario );
+		
+		
+	}
+
+}
---
+++ exercicio2.java
@@ -0,0 +1,25 @@
+package exercicio1;
+
+import java.util.Scanner;
+
+public class exercicio2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner leia = new Scanner(System.in);
+	    float n1, n2, n3, n4, media;
+	    
+	    System.out.print("Digite a Primeira nota: ");
+	    n1 = leia.nextFloat();
+	    System.out.print("Digite a Segunda nota: ");
+	    n2 = leia.nextFloat();
+	    System.out.print("Digite a Terceira nota: ");
+	    n3 = leia.nextFloat();
+	    System.out.print("Digite a Quarta nota: ");
+	    n4 = leia.nextFloat();
+	    media = (n1+n2+n3+n4)/4;
+	    System.out.println("A média de pontos do aluno é: "+ media);
+	}
+
+}
